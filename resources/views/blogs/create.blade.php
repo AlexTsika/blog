f449b7e68f5e3f4b6b79a90baccc2db7042e79ec
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-05-29 18:39:22 +0200

    added some bootstrap styling

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -1,14 +1,36 @@
-<h1>Create a new blog post</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body class="bg-dark text-light">
 
-<form method="POST" action="/blogs">
-    @csrf
-    <div>
-        <label for="title">Title</label>
-        <input type="text" id="title" name="title" required>
+    <div class="container-fluid">
+        <h1 class="text-center mt-5 mb-5">Create New Post</h1>
+
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <form method="POST" action="/blogs" class="needs-validation" novalidate>
+                    @csrf
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="title" name="title" required>
+                        <div class="invalid-feedback">Please provide a title.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="content" class="form-label">Content</label>
+                        <textarea class="form-control" id="content" name="content" required></textarea>
+                        <div class="invalid-feedback">Please provide some content.</div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>            
+            </div>
+        </div>
     </div>
-    <div>
-        <label for="content">Content</label>
-        <textarea id="content" name="content" required></textarea>
-    </div>
-    <button type="submit">Create Post</button>
-</form>
+
+</body>
+</html>
--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,24 +1,49 @@
-<a href="/blogs/create">Create a new post</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body class="bg-dark text-light">
 
-@if (session('status'))
-    <div class="alert alert-success">
-        {{ session('status') }}
+    <div class="container-fluid">
+        <h1 class="text-center mt-5 mb-5">Blog</h1>
+
+        <div class="d-flex justify-content-center mb-5">
+            <button type="button" class="btn btn-primary" onclick="window.location.href='/blogs/create'">Create new post</button>
+        </div>
+
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+    
+        @if($blogs->isEmpty())
+            <p class="text-center">No posts yet.</p>
+        @else
+        <div class="row">
+            
+        </div>
+            @foreach ($blogs->reverse() as $blog)
+                <div class="row justify-content-center mb-5">
+                    <div class="col-md-4">
+                        <h5>{{ $blog->title }}</h5>
+                        <p>{{ $blog->content }}</p>
+                        <form method="POST" action="/blogs/{{ $blog->id }}" onsubmit="return confirm('Do you really want to delete this post?');">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </div>
+                </div>
+                
+            @endforeach
+        @endif    
     </div>
-@endif
 
-@if($blogs->isEmpty())
-    <p>No posts yet. <a href="/blogs/create">Create a new post</a></p>
-@else
-    @foreach ($blogs as $blog)
-        <h2>{{ $blog->title }}</h2>
-        <p>{{ $blog->content }}</p>
-        
-        <!-- Delete form -->
-        <form method="POST" action="/blogs/{{ $blog->id }}" onsubmit="return confirm('Do you really want to delete this post?');">
-            @csrf
-            @method('DELETE')
-            <button type="submit">Delete</button>
-        </form>
-        
-    @endforeach
-@endif
+</body>
+</html>
